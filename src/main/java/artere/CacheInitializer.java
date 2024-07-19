52771018382d00db473f4d0b8ac4c876556038a3
Souheib <souheib.bousalem@eadn.dz>
2024-07-19 19:56:32 +0100

    2nd commit

--- src/main/java/artere/CacheInitializer.java
+++ src/main/java/artere/CacheInitializer.java
@@ -4,8 +4,10 @@ import jakarta.enterprise.context.ApplicationScoped;
 
 import jakarta.annotation.PostConstruct;
 import jakarta.inject.Inject;
+import jakarta.inject.Singleton;
 
-@ApplicationScoped
+
+@Singleton
 public class CacheInitializer {
 
     @Inject
@@ -15,23 +17,23 @@ public class CacheInitializer {
     public void init() {
         // Créer des objets Person
         Person person1 = new Person();
-        person1.setFirstname("John");
-        person1.setLastname("Doe");
+        person1.setFirstname("Souheib");
+        person1.setLastname("BOUSALEM");
 
         Person person2 = new Person();
-        person2.setFirstname("Jane");
-        person2.setLastname("Smith");
+        person2.setFirstname("Aryem");
+        person2.setLastname("BOUSALEM");
 
         // Créer des objets CacheElement
         CacheElement element1 = new CacheElement();
-        element1.setKey("person1");
+        element1.setKey("000001");
         element1.setValue(person1);
-        element1.setTtl(120); // TTL de 120 secondes
+        element1.setTtl(120);
 
         CacheElement element2 = new CacheElement();
-        element2.setKey("person2");
+        element2.setKey("00002");
         element2.setValue(person2);
-        element2.setTtl(180); // TTL de 180 secondes
+        element2.setTtl(180);
 
         // Ajouter les éléments au cache
         cacheService.setCache(element1);
